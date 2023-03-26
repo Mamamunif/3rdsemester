749538fc8e4880c63744fb54548c0c88ae65914c
Ubuntu <ubuntu@ip-172-31-13-130.ec2.internal>
2023-03-26 01:02:02 +0000

    1st commit

---
+++ ingress-rule/ingress-rule.tf
@@ -0,0 +1,62 @@
+resource "kubernetes_ingress_v1" "voting-ingress" {
+  metadata {
+    name      = "azure-vote-front"
+    labels = {
+      name = "azure-vote-front"
+    }
+    annotations = {
+      "kubernetes.io/ingress.class" : "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = "vote.jamilahoyiza.me"
+      http {
+        path {
+          backend {
+            service{
+              name = "azure-vote-front"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "micro-ingress" {
+  metadata {
+    name      = "sock-shop"
+    namespace = "sock-shop"
+    labels = {
+      name = "front-end"
+    }
+    annotations = {
+      "kubernetes.io/ingress.class" : "nginx"
+    }
+  }
+
+  spec {
+    rule {
+      host = "sock-shop.jamilahoyiza.me"
+      http {
+        path {
+          backend {
+            service{
+              name = "front-end"
+              port {
+                number = 80
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+
